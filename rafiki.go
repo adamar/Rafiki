61eefdcf0f15c9eb3d8e7ea74189a494c3d98d8b
Andrew Martin <a.a.d.martin@gmail.com>
2014-09-11 11:06:00 +1000

    fmt cleanup

--- rafiki.go
+++ rafiki.go
@@ -1,94 +1,90 @@
 package main
 
 import (
-        //"encoding/base64"
-	    "flag"
-        "log"
-        "database/sql"
-	    _ "github.com/mattn/go-sqlite3"
-        "fmt"
-        "os"
+	//"encoding/base64"
+	"database/sql"
+	"flag"
+	"fmt"
+	_ "github.com/mattn/go-sqlite3"
+	"log"
+	"os"
 )
 
 func main() {
 
-
 	var (
-                dbFile = flag.String("db", "rafiki.db", "Location of Your Rafiki DB File")
+		dbFile = flag.String("db", "rafiki.db", "Location of Your Rafiki DB File")
 	)
 
-        // Check for SQLite Database, if unfound prompt to create
-        //
+	// Check for SQLite Database, if unfound prompt to create
+	//
 	flag.Parse()
-        log.Print(dbFile)
+	log.Print(dbFile)
 	if flag.NFlag() == 0 {
-           var i string
-           fmt.Println("No DB specified, y/n to create a new one")
-           fmt.Scan(&i)
-           if i == "y" {
-              CreateDB()
-           } else {
-              os.Exit(0)
-           }
+		var i string
+		fmt.Println("No DB specified, y/n to create a new one")
+		fmt.Scan(&i)
+		if i == "y" {
+			CreateDB()
+		} else {
+			os.Exit(0)
+		}
 	}
 
-        // Open DB Conn
-        //
-        db, err := sql.Open("sqlite3", *dbFile)
-        errHandler(err)
-        defer db.Close()
+	// Open DB Conn
+	//
+	db, err := sql.Open("sqlite3", *dbFile)
+	errHandler(err)
+	defer db.Close()
 
-        listKeys(db)
+	listKeys(db)
 }
 
 func insertKeys(db *sql.DB) {
 
-        _, err := db.Exec("INSERT INTO csrs (cn, csr) VALUES ('foo', 'asxasxas')")
-        errHandler(err)
+	_, err := db.Exec("INSERT INTO csrs (cn, csr) VALUES ('foo', 'asxasxas')")
+	errHandler(err)
 
 }
-
 
 func listKeys(db *sql.DB) {
 
-        rows, err := db.Query("select id, cn, csr from csrs")
-        if err != nil {
-                log.Fatal(err)
-        }
-        defer rows.Close()
-        for rows.Next() {
-                var id int
-                var name string
-                rows.Scan(&id, &name)
-                fmt.Println(id, name)
-        }
-        rows.Close()
+	rows, err := db.Query("select id, cn, csr from csrs")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+	for rows.Next() {
+		var id int
+		var name string
+		rows.Scan(&id, &name)
+		fmt.Println(id, name)
+	}
+	rows.Close()
 }
 
-
 func errHandler(err error) {
-        if err != nil {
-                log.Print(err)
-        }
+	if err != nil {
+		log.Print(err)
+	}
 }
 
 func CreateDB() {
 
-        // Create Database File
-        //
-        db, err := sql.Open("sqlite3", "./rafiki.db")
-        errHandler(err)
-        defer db.Close()
+	// Create Database File
+	//
+	db, err := sql.Open("sqlite3", "./rafiki.db")
+	errHandler(err)
+	defer db.Close()
 
-
-        // Generate Schema for DB
-        //
-        sqlStmt := `
+	// Generate Schema for DB
+	//
+	sqlStmt := `
         create table csrs (id integer not null primary key, 
                           cn text,
                           csr blob);
         `
-        _, err = db.Exec(sqlStmt)
-        errHandler(err)
+	_, err = db.Exec(sqlStmt)
+	errHandler(err)
 
 }
