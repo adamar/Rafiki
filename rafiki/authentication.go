7ec490c98d84c75c106dc5a5f3237ee19209f172
Andrew Martin <a.a.d.martin@gmail.com>
2014-10-03 16:56:24 +1000

    moved more funcs

--- rafiki/authentication.go
+++ rafiki/authentication.go
@@ -11,3 +11,30 @@ func checkPassword() (passwd string, err error){
 
 }
 
+
+
+func setPassword() (passwd string, err error){
+
+    pass_attempt_one, err := gopass.GetPass("Please enter your new Password:")
+    if err != nil {
+        return "", err
+    }
+
+    pass_attempt_two, err := gopass.GetPass("Please re-enter your new Password:")
+    if err != nil {
+        return "", err
+    }
+
+    if pass_attempt_one != pass_attempt_two {
+
+        passwd := ""
+        err = fmt.Errorf("Passwords dont match")
+        return passwd, err
+
+    } else {
+
+        return pass_attempt_one, err
+    }
+}
+
+
