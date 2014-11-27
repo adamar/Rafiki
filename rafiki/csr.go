069eb17f8ab6fb18e1eb896d0313ad930f707f62
Andrew Martin <a.a.d.martin@gmail.com>
2014-11-28 08:18:03 +1100

    removed old funcs

--- rafiki/csr.go
+++ rafiki/csr.go
@@ -9,27 +9,6 @@ import (
 	"os"
 )
 
-func ExportCSR(c *cli.Context, db *sql.DB, password string) {
-
-    err := CheckFileFlag(c)
-    if err != nil {
-        log.Print(err)
-    }
-
-	keyname := GetKeyName()
-    log.Print(keyname)
-
-	ciphertext := SelectKey(db, keyname)
-
-	cleartext, err := DecryptString([]byte(password), ciphertext)
-        err = ioutil.WriteFile(c.String("file"), []byte(cleartext), 0644)
-        if err != nil {
-            panic(err)
-        }
-
-}
-
-
 
 
 func checkCSRFileSet(value bool) {
