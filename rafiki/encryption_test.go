c0421f1e7f09898c7b2165e23923e0e2deaa05fe
Andrew Martin <a.a.d.martin@gmail.com>
2015-01-08 15:44:38 +1100

    Adding decrypt test

--- rafiki/encryption_test.go
+++ rafiki/encryption_test.go
@@ -5,6 +5,7 @@ package rafiki
 import  (
         "testing"
         "strings"
+        "fmt"
         )
 
 func TestEncryptString(t *testing.T) {
@@ -17,6 +18,8 @@ func TestEncryptString(t *testing.T) {
         t.Error("Encrypt String Failed")
     }
 
+    fmt.Printf(output)
+
     expected_prefix := `-----BEGIN PGP SIGNATURE-----`
 
     if strings.HasPrefix(output, expected_prefix)  != true {
@@ -26,3 +29,23 @@ func TestEncryptString(t *testing.T) {
 }
 
 
+//func TestDecryptString(t *testing.T) {
+
+
+    encrypted_string := `-----BEGIN PGP SIGNATURE-----
+
+wx4EBwMCqZVMs/V7IV1gtsRHjTqbn8S3vXyYGd/Yd9HS4AHkzUsIvAqMuowNAwS0
+ocirtuGEgOCw4Cnh9Ivg/+KMvOah4N7kvlyAqXsJrVaejXWjmLbHM+D141+imeSf
+jRrR4JTiMQpe2eCa5JakEfxBOb2pw1ud0q8g+gjiR+eWGuEfjwA=
+=W3OG
+-----END PGP SIGNATURE-----`
+ 
+
+    key := []byte("My Encryption Key")
+
+
+    output, err := DecryptString(key, encrypted_string)
+
+
+
+}
