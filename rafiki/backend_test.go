0d44b06aad58876cd5b205a53e5ed4c34bb584be
Andrew Martin <a.a.d.martin@gmail.com>
2015-01-20 14:49:18 +1100

    begin adding db tests

---
+++ rafiki/backend_test.go
@@ -0,0 +1,32 @@
+
+package rafiki
+
+
+import  (
+        "testing"
+        "os"
+        )
+
+
+func TestCreateDBConn(t *testing.T) {
+
+    dbFileName := "test_rafiki_db_name"
+
+    db := createDBConn(dbFileName)
+    defer db.Close()
+
+    var now string
+    err := db.QueryRow("select 1").Scan(&now)
+    if err != nil {
+       t.Error(err)
+    } 
+
+    if now != "1" {
+        t.Error("SQLite Error")
+    }
+
+    os.Remove(dbFileName)
+
+}
+
+
