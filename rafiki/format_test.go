6e9e9d0fe641bb072f549980875f832bafcb7f06
Andrew Martin <a.a.d.martin@gmail.com>
2015-07-31 20:52:31 +1000

    Added sha256 test

--- rafiki/format_test.go
+++ rafiki/format_test.go
@@ -10,7 +10,7 @@ func Testmd5String(t *testing.T) {
 	output := md5String(input)
 
 	if output != "6f8db599de986fab7a21625b7916589c" {
-		t.Error("MD5 Formatting failed")
+		t.Error("MD5 Calculation failed")
 	}
 
 }
@@ -22,6 +22,17 @@ func TestformatMd5(t *testing.T) {
 
 	if output != "6f:8d:b5:99:de:98:6f:ab:7a:21:62:5b:79:16:58:9c" {
 		t.Error("MD5 Formatting failed")
+	}
+
+}
+
+func TesthashStringToSHA256(t *testing.T) {
+
+	input := "test string"
+	output := hashStringToSHA256(input)
+
+	if output != "d5579c46dfcc7f18207013e65b44e4cb4e2c2298f4ac457ba8f82743f31e930b" {
+		t.Error("sha256 Calculation failed")
 	}
 
 }
